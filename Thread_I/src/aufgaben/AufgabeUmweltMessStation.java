5ee8c7eba80926cc36ab25ac99726a07c2e0a870
Christian Schirmer <christian.schirmer@paufa.de>
2022-02-08 13:03:12 +0100

    Added AufgabeUmweltmessStation

---
+++ Thread_I/src/aufgaben/AufgabeUmweltMessStation.java
@@ -0,0 +1,34 @@
+package aufgaben;
+
+import java.util.Random;
+
+/**
+ * Messung von Feinstaubwerten:
+ * 
+ * In einem Thread werden alle 100 Millisekunden Feinstaubwerte gemessen.
+ * 
+ * Simuliert wird jede Messung durch das Generieren einer Zufallszahl, die im
+ * Bereich des vorherigen Wertes +/- 8 Mikrogramm liegt. Die Anfangswert kann
+ * auf 15 Mikrogramm gesetzt werden.
+ * 
+ * 
+ * In einem anderen Thread werden die drei letzten vorhandenen Messungen
+ * analysiert (1 mal pro Sekunde). Falls in diesen drei Messungen der stätige
+ * Abfall erkannt wird, wird die Tendenz auf der Konsole visualisiert. Dasselbe
+ * zur stätigen Steigung.
+ * 
+ * 
+ * Im dritten Thread werden Mitteilungen ausgegeben, falls die aktuelle
+ * Feinstaubkonzentration einen Grenzwert überschreitet (>50) (Erhöhte
+ * Belastung) oder unterschreitet (<8)(Sehr saubere Luft). Auch die Kontrollen
+ * werden nur periodisch (1 Mal in 2 Sek) durchgeführt
+ * 
+ *
+ */
+public class AufgabeUmweltMessStation {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		rand.nextInt(150);
+	}
+}
